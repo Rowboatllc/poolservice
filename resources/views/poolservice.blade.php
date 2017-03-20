d5c77d59b00a2aa27e28815c0e332618dd095818
sangcao <sang@rowboatsoftware.com>
2017-03-20 10:05:53 +0700

    add multi tabs for pool service

--- resources/views/poolservice.blade.php
+++ resources/views/poolservice.blade.php
@@ -34,37 +34,74 @@
         </div>
     </div>
 </div>-->
-
-
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-<div>
-        <div id="tabs">
-        <ul>
-            <li><a href="#fragment-1"><span>One</span></a></li>
-            <li><a href="#fragment-2"><span>Two</span></a></li>
-            <li><a href="#fragment-3"><span>Three</span></a></li>
-        </ul>
-        <div id="fragment-1">
-            <p>First tab is active by default:</p>
-            <pre><code>$( "#tabs" ).tabs(); </code></pre>
-        </div>
-        <div id="fragment-2">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-        </div>
-        <div id="fragment-3">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-        </div>
+  <h2>Pool service</h2>
+  <p>To make the tabs toggleable, add the data-toggle="tab" attribute to each link. Then add a .tab-pane class with a unique ID for every tab and wrap them inside a div element with class .tab-content.</p>
+
+  <ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
+    <li><a data-toggle="tab" href="#menu1">Menu 1</a></li>
+    <li><a data-toggle="tab" href="#menu2">Menu 2</a></li>
+    <li><a data-toggle="tab" href="#menu3">Menu 3</a></li>
+  </ul>
+
+  <div class="tab-content">
+    <div id="home" class="tab-pane fade in active">
+      <h3>HOME</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </div>
- </div>
+    <div id="menu1" class="tab-pane fade">
+      <h3>Menu 1</h3>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </div>
+    <div id="menu2" class="tab-pane fade">
+      <h3>Menu 2</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+    <div id="menu3" class="tab-pane fade">
+      <h3>Menu 3</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+  </div>
 </div>
 <script>
-    $( "#tabs" ).tabs();
+    // $(function() {
+    //     $(".nexttab").click(function() {
+    //         alert('hahahahahahaha');
+    //     });
+    // })
+    // var validator = $("#myForm").validate();
+    
+    // var tabs = $("#tabs").tabs({
+    //     select: function(event, ui) {
+    //         alert('hahahahahahaha');
+    //         var valid = true;
+    //         var current = $(this).tabs("option", "selected");
+    //         var panelId = $("#tabs ul a").eq(current).attr("href");
+            
+    //         $(panelId).find("input").each(function() {
+    //             console.log(valid);
+    //             if (!validator.element(this) && valid) {
+    //                 valid = false;
+    //             }
+    //         });
+
+    //         return valid;
+    //     }
+    // });
+
+
+    // $(".nexttab").click(function() {
+    //     alert('hahahahahahaha1');
+    //     $("#tabs").tabs("select", this.hash);
+    // });
+
+    // //use link to submit form instead of button
+    // $("a[id=submit]").click(function() {
+    //     alert('hahahahahahaha2');
+    //     $(this).parents("form").submit();
+    // });
+    // $( "#tabs" ).tabs();
 </script>
 @endsection
 
