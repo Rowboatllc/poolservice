a3b6c3d315f40d4665f3b265848c0f0cd59b8a87
lnrowboat <lap@rowboatsoftware.com>
2017-03-27 11:36:11 +0700

    get token

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\PageRepositoryInterface;
+use App\Http\Requests\PageRequest;
+class PageController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(PageRepositoryInterface $page)
+    {
+        $this->page=$page;
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pages = $this->page->getAllPage();
+        if(isset($pages) && count($pages)>0){
+            $page = $pages[0];
+        }
+        return view('admin.page', compact('pages','page'));
+    }
+
+    public function store(PageRequest $request){
+        $alias = $request->input('alias');
+        $title = $request->input('title');
+        $content = $request->input('content');
+        $keywords = $request->input('keywords');
+        $description = $request->input('description');
+        $result = $this->page->createOrUpdatePage($alias, $title, $content, $keywords, $description);
+        if($result)
+            return redirect()->back()
+                        ->withInput($request->all())
+                        ->with('success', true);
+
+        return redirect()->back()
+                        ->withInput($request->all())
+                        ->with('error', true);
+    }
+
+    public function getPage(Request $request){
+        $alias = $request->input('alias');
+        return $alias;
+    }
+
+
+}
