aae918ef52763d5354a7e0d49661489021f32ec6
lnrowboat <lap@rowboatsoftware.com>
2017-03-30 14:40:55 +0700

    Test acl, example delay request when key up

--- app/Repositories/AclRepository.php
+++ app/Repositories/AclRepository.php
@@ -10,114 +10,114 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 
 class AclRepository {
-    
-    private $user;
-    private $group;
-    private $permission;
-    
-    public function __construct(User $user, Group $group, Permission $permission ) {
+
+    public $user;
+    public $group;
+    public $permission;
+
+    public function __construct(User $user, Group $group, Permission $permission) {
         $this->user = $user;
         $this->group = $group;
         $this->permission = $permission;
     }
-    
-    public function createGroup($name, $description='') {
-        $find = $this->group->where('name',$name)->first();
-        if(isset($find)){
-            var_dump(1); 
+
+    public function createGroup($name, $description = '') {
+        $find = $this->group->where('name', $name)->first();
+        if (!empty($find)) {
             return false;
-        }else{
-            $group = new Group();
-            $group->name = $name;
-            $group->description = $description;
-            return  $group->save();
+        } else {
+            $this->group->name = $name;
+            $this->group->description = $description;
+            return $group->save();
         }
     }
-    
+
     public function attachUserToGroup($group_id, $user_id) {
         $group = $this->group->find($group_id);
         $user = $this->user->find($user_id);
-        if(isset($group)&&isset($user)){
-            if(!$group->users->contains($users->id))
+        if (!empty($group) && !empty($user)) {
+            if (!$group->users->contains($user->id))
                 return $group->users()->attach($user->id);
+            return 1;
         }
-        return false;        
+        return 0;
     }
-    
+
     public function dettachUserFromGroup($group_id, $user_id) {
         $group = $this->group->find($group_id);
-        if(isset($group)){
-            if(!$group->users->contains($user_id))
+        if (!empty($group)) {
+            if (!$group->users->contains($user_id))
                 return 1;
             return $group->users()->detach($user_id);
         }
         return 0;
     }
-    
+
     static public function userGetPermissions($user_id) {
         // $user = Auth::user();
-        if(isset($user_id)){
+        if (!empty($user_id)) {
             $results = DB::select('SELECT p.alias FROM permissions p 
                                 LEFT JOIN group_permission gp ON p.id=gp.permission_id 
                                 LEFT JOIN groups g ON gp.group_id = g.id
                                 WHERE g.id  IN (
                                     SELECT ug.group_id FROM users u
                                     LEFT JOIN user_group ug ON u.id = ug.user_id
-                                    WHERE u.id = '.$user_id.'
+                                    WHERE u.id = ' . $user_id . '
                                 )');
             $alias = [];
-            foreach($results as $permission){
+            foreach ($results as $permission) {
                 $alias[] = $permission->alias;
             }
             return $alias;
         }
         return [];
     }
-    
-    static public function userHasPermission($user_id, $alias) {
-        $aliass = self::userGetPermissions($user_id);
-        return in_array($alias,$aliass);
+
+    static public function userHasPermission($user_id, $permission) {
+        $permissions = self::userGetPermissions($user_id);
+        return in_array($permission, $permissions);
     }
-    
-    public function createPermission($name, $alias, $description='') {
-        $find = $this->permission->where('alias',$alias)->first();
-        if(isset($find)){
+
+    public function createPermission($name, $alias, $description = '') {
+        $find = $this->permission->where('alias', $alias)->first();
+        if (!empty($find)) {
             return 0;
-        }else{
+        } else {
             $permission = new Permission();
             $permission->name = $name;
             $permission->alias = $alias;
             $permission->description = $description;
-            return  $permission->save();
+            return $permission->save();
         }
     }
-    
+
     public function groupAttachPermissions($group_id, $permission_id) {
         $group = $this->group->find($group_id);
         $permission = $this->permission->find($permission_id);
-        if(isset($group)&&isset($permission)){
-            if(!$group->permissions->contains($permission->id))
-                return $group->permissions()->attach($permission->id);
+        if (!empty($group) && !empty($permission)) {
+            if (!$group->permissions->contains($permission_id))
+                return $group->permissions()->attach($permission_id);
+            return 1;
         }
         return 0;
     }
-    
+
     public function groupDettachPermissions($group_id, $permission_id) {
         $group = $this->group->find($group_id);
-        if(isset($group)){
-            if(!$group->permissions->contains($permission_id))
+        if (!empty($group)) {
+            if (!$group->permissions->contains($permission_id))
                 return 1;
             return $group->permissions()->detach($permission_id);
         }
         return 0;
     }
-    
+
     public function cachePermission($permissions) {
-        Session::put('user_permission',$permissions);
+        Session::put('user_permission', $permissions);
     }
 
     public function deleteCachePermission() {
         Session::forget('user_permission');
     }
-    
+
 }
--- tests/Unit/AclRepositoryTest.php
+++ tests/Unit/AclRepositoryTest.php
@@ -5,43 +5,68 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Database\Eloquent\Model;
-use App\Repositories\AclRepository;
-use Faker;
-use App\Models\User;
-use App\Models\Group;
-use App\Models\Permission;
+//use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class AclRepositoryTest extends TestCase{
+class AclRepositoryTest extends TestCase {
 
     private $repository;
     private $faker;
-    
+
     public function __construct() {
-        $user = new User; $group = new Group ;$permission = new Permission;
-        $this->repository = new AclRepository($user, $group, $permission);
-        $this->faker = Faker\Factory::create();
+        $user = new \App\Models\User;
+        $group = new \App\Models\Group;
+        $permission = new \App\Models\Permission;
+        $this->repository = new \App\Repositories\AclRepository($user, $group, $permission);
+        $this->faker = \Faker\Factory::create();
     }
 
-    public function test_permission(){
-        $group_name = 'Test';
-        $group_description = 'Test description';
-        $this->repository->createGroup($group_name,$group_description);
-        
-        // $user_admin = factory(App\Models\User::class)->make();
+    public function createDummyData() {
+        $this->repository->createGroup('gp1', 'group1');
+        $this->repository->createGroup('gp2', 'group2');
+        $this->repository->createGroup('gp3', 'group3');
+        $this->repository->createPermission('permission 1', 'psm1');
+        $this->repository->createPermission('permission 2', 'psm2');
+        $this->repository->createPermission('permission 3', 'psm3');
+        $this->repository->createPermission('permission 4', 'psm4');
+        $this->repository->user->create([
+            'email' => $this->faker->email,
+            'password' => \Hash::make($this->faker->password),
+            'status' => 'active'
+        ]);
     }
 
-    public function test_get_permission()
-    {
-        // $result = $this->repository->userGetPermissions(7);
-        // $this->assertNotEmpty($result);
+    public function test_attach_detachPermission() {
+        //$this->createDummyData();
+
+        $group = $this->repository->group->first();
+        $permission = $this->repository->permission->first();
+        $user = $this->repository->user->first();
+
+        $result = $this->repository->groupAttachPermissions($group->id, $permission->id);
+        if ($result === null)
+            $result = 1;
+        $this->assertEquals($result, 1);
+
+        $result = $this->repository->attachUserToGroup($group->id, $user->id);
+        if ($result === null)
+            $result = 1;
+        $this->assertEquals($result, 1);
+
+        $result = $this->repository->userGetPermissions($user->id);
+        $this->assertNotEmpty($result);
+
+        $result = $this->repository->userHasPermission($user->id, $permission->alias);
+        $this->assertTrue($result);
+
+        $result = $this->repository->groupDettachPermissions($group->id, $permission->id);
+        if ($result === null)
+            $result = 1;
+        $this->assertEquals($result, 1);
+
+        $result = $this->repository->dettachUserFromGroup($group->id, $user->id);
+        if ($result === null)
+            $result = 1;
+        $this->assertEquals($result, 1);
     }
 
-    public function test_has_permission()
-    {
-        // $result = $this->repository->userHasPermission(7,'delete.admin');
-        // $this->assertTrue($result);
-    }
 }
-
