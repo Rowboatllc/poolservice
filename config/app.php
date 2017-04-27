82b4ffb427f9e7b7f7ecdd386d6708d870ca470d
lnrowboat <lap@rowboatsoftware.com>
2017-04-27 10:29:42 +0700

    config

--- config/app.php
+++ config/app.php
@@ -60,7 +60,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'dev.poolservice.com'),
+    'url' => env('APP_URL', 'http://dev.poolservice.com'),
 
     /*
     |--------------------------------------------------------------------------
