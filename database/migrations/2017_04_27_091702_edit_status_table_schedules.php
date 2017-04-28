758bce1a12a4d7c6fdafebede048a0d6369677b9
sangcao <sang@rowboatsoftware.com>
2017-04-28 09:47:29 +0700

    hot fixed issues migrate code

--- database/migrations/2017_04_27_091702_edit_status_table_schedules.php
+++ database/migrations/2017_04_27_091702_edit_status_table_schedules.php
@@ -14,7 +14,7 @@ class EditStatusTableSchedules extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('name', 50)->change();
+            // $table->string('name', 50)->change();
         });
     }
 
